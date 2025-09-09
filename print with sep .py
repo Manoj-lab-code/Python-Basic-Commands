0e6c80e8e5c6e8f3770688a9c98315517540a69e
Manoj <manoj.discovered@gmail.com>
2025-09-09 21:19:01 +0100

    Add files via upload

---
+++ print with sep .py
@@ -0,0 +1,8 @@
+#Display three string “Name”, “Is”, “Python” where every word is seperated by “**” using the keyword "sep".
+#The output should be displayed as below:
+#“Name**Is**Python"
+
+
+print("Name","Is","Python",sep="**")
+print("Hello")
+print("python is easy")
