45c066797b47c09e4fb47c2fb7943882694c1780
Manoj <manoj.discovered@gmail.com>
2025-09-09 21:30:15 +0100

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,29 @@
+sport_brands = 'yonex, nike, addidas, victor' #sport_brand is a variable containing a list of sport brands
+
+print(sport_brands) #printing the sports brand
+
+yonex, nike, addidas, victor
+
+sport_brands = ['yonex', 'nike', 'addidas', 'victor']
+
+print(sport_brands[1])
+
+nike
+
+sport_brands[3]='jordans'
+
+print(sport_brands)
+
+['yonex', 'nike', 'addidas', 'jordans']
+
+sport_brands.append('victor')
+
+print(sport_brands)
+
+['yonex', 'nike', 'addidas', 'jordans', 'victor']
+
+del sport_brands[2]
+
+print(sport_brands)
+
+['yonex', 'nike', 'jordans', 'victor']
