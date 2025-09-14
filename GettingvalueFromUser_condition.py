30457899e91630e2a8aef050da4a78885c1d77e7
Manoj <manoj.discovered@gmail.com>
2025-09-14 20:38:07 +0100

    Add files via upload

---
+++ GettingvalueFromUser_condition.py
@@ -0,0 +1,8 @@
+#Write a program to check if they have greater than 25 dollars, they get a apple ear pods or they get a normal headsets.
+
+
+money = int(input("enter amount of money"))
+if money>25:
+  print("you can get apple ear pods")
+else:
+  print("you can get a normal headset")
